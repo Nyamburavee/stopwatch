41f009fdada855d2fcc24c3071daa7a02cfff1e9
[Nyamburavee] <[veronyambura716@gmail.com]>
2024-07-24 20:58:59 +0300

    stopwatch timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [timer, setTimer] = React.useState(0)
+    
+    function timeCount (){
+        setTimer((timer) => timer + 1)
+        
+    }
+   /* const startTimer = () => {
+            window.myTimer = setInterval(() =>{
+                setTimer((timer) => timer + 1);
+            }, 1000)
+            
+            
+    };*/
+    const startTimer = ()=>{
+        window.myTimer = setInterval(timeCount, 1000);
+    }
+    
+    const stopTimer = () =>{
+        if(window.myTimer){
+        clearInterval(window.myTimer)};
+    };
+    
+    const resetTimer = () =>{
+        clearInterval(window.myTimer);
+        setTimer(0);
+    };
+    
+    return (
+        <div className="container flex flex-col justify-center items-center
+            p-10 rounded-lg bg-green-300 mt-20 w-[450px] ">
+          <h1 className=" text-3xl font-bold text-white mb-8"> Stopwatch Timer</h1>
+          <span className=" text-white font-bold text-xl">{timer % 60} Secs</span>
+          <span className=" text-white font-bold text-xl mb-4">{Math.trunc(timer / 60)} Mins </span>
+          <div className=" flex flex-col justify-center">
+            <button onClick={startTimer}>Start</button>
+            <button onClick={stopTimer}>Stop</button>
+            <button onClick={resetTimer}>Reset</button>
+          </div>
+
+
+        </div>
+    )
 }
-
-export default App;
